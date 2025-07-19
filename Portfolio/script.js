9b5d4f594ef8b2cf29d8a8f402f27ff9884c962e
RAMKRISHNA MODAK <154114073+Ramkrisn25@users.noreply.github.com>
2025-07-19 18:02:18 +0530

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.createElement('button');
-toggle.textContent = 'Toggle Dark Mode';
-toggle.style.margin = '20px';
-document.body.insertBefore(toggle, document.body.firstChild);
-
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-});
